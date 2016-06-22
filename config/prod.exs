df993f97b0a873d67c85c61d7393e3bb108b7fb8
Brandon Joyce <bjoyce@covermymeds.com>
2016-06-21 20:37:08 -0400

    Another oops

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :beer_napkin, BeerNapkin.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "beernapkin.com", port: {:system, "PORT"}],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info

5fda8575da5a76152dea8c4086d3dc08d8b1f98d
Brandon Joyce <bjoyce@covermymeds.com>
2016-06-22 19:25:39 -0400

    Updating production url config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :beer_napkin, BeerNapkin.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "beernapkin.com", port: {:system, "PORT"}],
+  url: [host: "www.beernapkin.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
